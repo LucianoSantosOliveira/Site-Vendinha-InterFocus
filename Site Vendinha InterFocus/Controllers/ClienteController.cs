e433d2d907a712931192838730f8541d37d5ebe6
Luciano <lucianoliv2@hotmail.com>
2023-01-26 20:08:36 -0300

    Crud Cliente

--- Site Vendinha InterFocus/Controllers/ClienteController.cs
+++ Site Vendinha InterFocus/Controllers/ClienteController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Site_Vendinha_InterFocus.Models;
+using System.Security.Policy;
+using System.Text;
 
 namespace Site_Vendinha_InterFocus.Controllers
 {
@@ -8,31 +11,39 @@ namespace Site_Vendinha_InterFocus.Controllers
     {
         private readonly HttpClient Request;
         // GET: ClienteController
-        public ActionResult Index()
+
+        public ClienteController()
         {
-            return View();
+            Request = new HttpClient();
+            Request.BaseAddress = new Uri("https://localhost:7200/");
         }
 
         // GET: ClienteController/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> DetailsAsync(Guid id)
         {
-            return View();
+            var clienteJson = await Request.GetStringAsync($"api/Clientes/{id.ToString()}");
+            var cliente = JsonConvert.DeserializeObject<Cliente>(clienteJson);
+            return View(cliente);
         }
 
         // GET: ClienteController/Create
         public ActionResult Create()
         {
-            return View();
+            var cliente = new Cliente() {ClienteId = Guid.NewGuid() };
+            return View(cliente);
         }
 
         // POST: ClienteController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> CreateClienteAsync(Cliente cliente)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var ClienteJson = JsonConvert.SerializeObject(cliente);
+                var requestContent = new StringContent(ClienteJson ,Encoding.UTF8, "application/json");
+                await Request.PostAsync("api/Clientes/", requestContent);
+                return RedirectToAction(nameof(ListAsync));
             }
             catch
             {
@@ -41,19 +52,24 @@ namespace Site_Vendinha_InterFocus.Controllers
         }
 
         // GET: ClienteController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> EditAsync(Guid id)
         {
-            return View();
+            var clienteJson = await Request.GetStringAsync($"api/Clientes/{id.ToString()}");
+            var cliente = JsonConvert.DeserializeObject<Cliente>(clienteJson);
+            return View(cliente);
         }
 
         // POST: ClienteController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> EditAsync(Guid id, Cliente cliente)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var ClienteJson = JsonConvert.SerializeObject(cliente);
+                var requestContent = new StringContent(ClienteJson, Encoding.UTF8, "application/json");
+                await Request.PutAsync($"api/Clientes/{cliente.ClienteId}", requestContent);
+                return RedirectToAction(nameof(ListAsync));
             }
             catch
             {
@@ -62,19 +78,22 @@ namespace Site_Vendinha_InterFocus.Controllers
         }
 
         // GET: ClienteController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> DeleteAsync(Guid id)
         {
-            return View();
+            var clienteJson = await Request.GetStringAsync($"api/Clientes/{id.ToString()}");
+            var cliente = JsonConvert.DeserializeObject<Cliente>(clienteJson);
+            return View(cliente);
         }
 
         // POST: ClienteController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> DeleteAsync(Guid id, Cliente cliente)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                await Request.DeleteAsync($"api/Clientes/{id}");
+                return RedirectToAction(nameof(ListAsync));
             }
             catch
             {
@@ -82,9 +101,11 @@ namespace Site_Vendinha_InterFocus.Controllers
             }
         }
 
-        public ActionResult List()
+        public async Task<ActionResult> ListAsync()
         {
-            return View(new List<Cliente>() { });
+            var clienteJson = await Request.GetStringAsync("api/Clientes");
+            var clientes = JsonConvert.DeserializeObject<List<Cliente>>(clienteJson);
+            return View(clientes);
         }
     }
 }
